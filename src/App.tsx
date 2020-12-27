3817b20f478ebb90c7ba91cb63bb9a7be818d91f
Thomas Lisankie <id@tomlisankie.com>
2020-12-27 18:03:03 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Rhizome Pub
-        </p>
-        <a
-          className="App-link"
-          href="https://roaman.pub"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Current Roaman Pub
-        </a>
-      </header>
-    </div>
-  );
+    let [productName, setProductName] = useState("Rhizome Pub");
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Rhizome Pub
+                </p>
+                <a
+                    className="App-link"
+                    href="https://roaman.pub/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Current: {productName}
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
